46ec2f71f9585b9966ef9c88a493bca7dec42dde
盒子 <hezi@hezidemacbook-air.local>
2018-06-05 15:17:31 +0800

    Initial Commit

---
+++ JDDAlertView-master/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JDDAlertView-master
+//
+//  Created by 盒子 on 2018/6/5.
+//  Copyright © 2018年 盒子. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JDDAlertView-master/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JDDAlertView-master
+//
+//  Created by 盒子 on 2018/6/5.
+//  Copyright © 2018年 盒子. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
