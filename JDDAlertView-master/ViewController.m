86ba93eec441a74c1afd12806d632873a0ad090a
盒子 <hezi@hezideMacBook-Air.local>
2018-06-05 15:35:43 +0800

    first commit

--- JDDAlertView-master/ViewController.m
+++ JDDAlertView-master/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "JDDAlertView.h"
 
 @interface ViewController ()
 
@@ -19,6 +20,22 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (IBAction)alertClick:(id)sender {
+    JDDAlertView *alert = [[JDDAlertView alloc] initJDDAlertWithTitle:nil andMessage:@"你准备好了吗？\n你真的准备好了吗?\n你真的真的准备好了吗?\n是的，我已经准备好啦\n哈哈哈"]
+    .leftBtnText(@"取消")
+    .rightBtnText(@"立刻登录")
+    .titleFontSize(20)
+    .messageFontSize(18)
+    .leftBtnTextColor([UIColor greenColor])
+    .rightBtnTextColor([UIColor redColor])
+    .leftBtnFontSize(18)
+    .rightBtnFontSize(18);
+    [alert show];
+    alert.alertHandelBlock = ^(NSInteger index, id data) {
+        NSLog(@"alert click index = %ld",(long)index);
+        
+    };
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
